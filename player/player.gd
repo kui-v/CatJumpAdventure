6a750d612fcfd0740cf0e606c103bef90f8afe5b
lentils <vancekuikahi@gmail.com>
2023-10-19 14:12:55 -0700

    trajectory wip

--- player/player.gd
+++ player/player.gd
@@ -4,9 +4,11 @@ extends RigidBody2D
 @onready var label_text : Label = get_node("UILayer/Label")
 @onready var label_text_2 : Label = get_node("UILayer/Label2")
 @onready var timer_label : Label = get_node("UILayer/TimerLabel")
+@onready var trajectory_line : Line2D = get_node("TrajectoryLine")
 
-var hold_time : float = 0
+var hold_percentage : float = 0.0
 var hold_force : float = 75
+var force : float = 0
 
 # hold button and release - force
 # 1. on button press
@@ -16,32 +18,54 @@ var hold_force : float = 75
 
 # process should only handle the timer; all physics should be done when action released
 func _process(_delta):
-	timer_label.text = str(hold_timer.time_left).pad_decimals(2)
-	calculate_trajectory(_delta)
-
-
-# using mouse and moving at the same time will make timer not work as expected
-func _input(event):
 	if Input.is_action_just_pressed("action_hold"):
 		hold_timer.start()
 	elif Input.is_action_just_released("action_hold"):
-		get_hold_velocity()
 		hold_timer.stop()
 		apply_movement()
+	else:
+		trajectory_line.show()
+		get_hold_velocity()
+		update_trajectory(_delta)
+	timer_label.text = str(hold_timer.time_left).pad_decimals(2)
 
 
 func get_hold_velocity():
-	hold_time = (hold_timer.wait_time - hold_timer.time_left)
+	var hold_time = (hold_timer.wait_time - hold_timer.time_left)
+	hold_percentage = hold_time / hold_timer.wait_time
+	force = hold_force * hold_percentage
 
 
 func apply_movement():
-	var force = hold_force * hold_time
+	var player_position = global_position
 	var direction = global_position.direction_to(get_global_mouse_position())
-	apply_impulse(direction * force)
+	apply_impulse(direction * force) #let's not use apply_impulse here
 	label_text.text = str(force)
 	label_text_2.text = str(direction)
 
 
-func calculate_trajectory(delta):
-	var position = global_position
-	print(position)
+func update_trajectory(delta):
+	trajectory_line.clear_points()
+	var max_points = 250
+	var pos = global_position
+	var vel = global_transform.x * force * global_position.direction_to(get_global_mouse_position())
+	for i in max_points:
+		trajectory_line.add_point(pos)
+#		vel.y += gravity_scale * mass * i * delta
+		pos += vel * delta
+	print(vel)
+	
+#	var pos = global_position
+#	var vel = global_transform.x * force
+#	for i in max_points:
+#		trajectory_line.add_point(pos)
+#		vel.y += gravity_scale * delta
+#		pos += vel * delta
+
+#	var current_position = global_position
+#	var trajectory_points = []
+#	for i in max_points:
+#		var impulse = force - gravity_scale * mass * i * delta
+#		current_position.y -= impulse * delta
+#		trajectory_points.append(current_position)
+#	trajectory_line.points = trajectory_points
